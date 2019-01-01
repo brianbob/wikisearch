e419743672a775e6670a7eb78a407bad1a4e2f6d
Brian Bridge <brian@Brians-MacBook-Pro.local>
2019-01-01 14:07:10 -0500

    second try at a drush command

---
+++ src/Commands/wikisearchCommands.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\wikisearch\Commands;
+
+use Drush\Commands\wikisearchCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ */
+class wikisearchCommands extends DrushCommands {
+  /**
+   * Echos back hello with the argument provided.
+   *
+   * @command wikisearch:wikiscrape
+   * @aliases wikiscrape
+   * @usage wikisearch:wikiscrape
+   *   Scrape wikipedia
+   */
+  public function wikiscrape() {
+    wikisearch_scrape();
+  }
+
+}
